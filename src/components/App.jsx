0c3fee679af4db12113b8461def3a9c21b746368
DeniOS777 <Denios777@gmail.com>
2022-08-21 15:22:54 +0300

    change files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { GlobalStyle } from './GlobalStyle';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <div>
+        <h1>Homework 03</h1>
+      </div>
+      <GlobalStyle />
     </div>
   );
 };
